085ac8c0a0366a6f11c1b5a70203de25a68655e2
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-22 20:13:27 +0100

    Added search bar and button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,28 @@
+import { Flex, IconButton, Input } from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const handleClick = () => {};
+
+  return (
+    <Flex justify="center">
+      <Flex p="5">
+        <Input
+          placeholder="Search for resturants.."
+          borderLeftRadius="full"
+          variant="filled"
+        />
+        <IconButton
+          onClick={handleClick}
+          icon={<FaSearch />}
+          borderRightRadius="full"
+          size="md"
+        >
+          Search
+        </IconButton>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default SearchBar;

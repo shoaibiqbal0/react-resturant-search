a85b3763ce68a3c0fc4d77aa3acc82f36cbc4af1
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-23 17:41:27 +0100

    Added search bar state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import MainText from "./MainText";
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
 
 const Main = () => {
+  const [search, setSearch] = useState("");
   return (
     <>
-      <SearchBar />
+      <SearchBar search={search} setSearch={setSearch} />
       <MainText />
       <SearchResults />
     </>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,19 @@
 import { Flex, IconButton, Input } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar = () => {
+const SearchBar = ({ search, setSearch }) => {
   const handleClick = () => {};
+  const handleChange = (event) => {
+    const searchText = event.target.value;
+    setSearch(searchText);
+  };
 
   return (
     <Flex p="5" justify="center">
       <Flex w="100%" maxW="40em">
         <Input
+          onChange={handleChange}
+          value={search}
           placeholder="Search for resturants.."
           borderLeftRadius="full"
           variant="filled"

aa741b3454561c50a2a1e2a9c2c94869e0f5adb7
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-24 16:10:46 +0100

    Created tiling

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,16 +1,49 @@
-import { Box, Flex, HStack, SimpleGrid, Text, VStack } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Flex,
+  HStack,
+  Image,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 const SearchResults = ({ searchResults, searchTerm, searchLocation }) => {
+  const badgeStyles = {
+    borderRadius: "7px",
+    fontSize: "0.6rem",
+    textTransform: "capitalize",
+  };
+
   return (
-    <VStack align="left" p="5">
+    <VStack align="left" p="1" mx="2">
       <Text>
-        {searchResults.total} results for {searchLocation} in {searchLocation}
+        Showing results for {searchTerm} in {searchLocation}
       </Text>
-      <SimpleGrid columns={[2, 2, 4]} gap="2">
-        <Box>Hello</Box>
-        <Box>Hello</Box>
-        <Box>Hello</Box>
-        <Box>Hello</Box>
+      <SimpleGrid columns={[1, null, 2]} gap="2">
+        <Box borderRadius="5" border="1px" borderColor="gray.200">
+          <HStack p="1" align="flex-start">
+            <Image
+              src={searchResults?.businesses[1]?.image_url}
+              maxW="5em"
+              borderRadius="5"
+            />
+            <VStack spacing="0">
+              <Text w="100%" fontWeight="bold">
+                {searchResults?.businesses[0]?.name}
+              </Text>
+              <Flex w="100%">
+                <Badge style={badgeStyles}>
+                  {searchResults?.businesses[0]?.categories[0]?.title}
+                </Badge>
+                <Badge style={badgeStyles}>
+                  {searchResults?.businesses[0]?.categories[1]?.title}
+                </Badge>
+              </Flex>
+            </VStack>
+          </HStack>
+        </Box>
       </SimpleGrid>
     </VStack>
   );

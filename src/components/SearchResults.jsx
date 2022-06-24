4ebd15bf842e1f6fde08d3982416eec8d9c6dc68
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-24 18:51:34 +0100

    Added some things

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -14,6 +14,7 @@ const SearchResults = ({ searchResults, searchTerm, searchLocation }) => {
     borderRadius: "7px",
     fontSize: "0.6rem",
     textTransform: "capitalize",
+    fontWeight: "normal",
   };
 
   return (
@@ -22,25 +23,51 @@ const SearchResults = ({ searchResults, searchTerm, searchLocation }) => {
         Showing results for {searchTerm} in {searchLocation}
       </Text>
       <SimpleGrid columns={[1, null, 2]} gap="2">
-        <Box borderRadius="5" border="1px" borderColor="gray.200">
-          <HStack p="1" align="flex-start">
+        <Box
+          borderRadius="5"
+          border="1px"
+          borderColor="gray.200"
+          boxShadow="lg"
+        >
+          <HStack p="1" align="flex-start" py="2">
             <Image
               src={searchResults?.businesses[1]?.image_url}
               maxW="5em"
               borderRadius="5"
             />
-            <VStack spacing="0">
-              <Text w="100%" fontWeight="bold">
+            <VStack spacing="0" w="100%" align="left">
+              <Text w="100%" fontWeight="bold" fontSize="2xl">
                 {searchResults?.businesses[0]?.name}
               </Text>
-              <Flex w="100%">
+              <HStack w="100%">
                 <Badge style={badgeStyles}>
                   {searchResults?.businesses[0]?.categories[0]?.title}
                 </Badge>
                 <Badge style={badgeStyles}>
                   {searchResults?.businesses[0]?.categories[1]?.title}
                 </Badge>
-              </Flex>
+
+                <Text fontSize="0.6rem">
+                  {searchResults?.businesses[0]?.price}
+                </Text>
+              </HStack>
+              <Text>
+                Rating:{" "}
+                {[
+                  ...Array(Math.floor(searchResults?.businesses[0]?.rating)),
+                ].map((item) => {
+                  console.log(item);
+                })}
+              </Text>
+              <Text pt="1" fontSize="0.8em">
+                <strong>Phone: </strong>
+                {searchResults?.businesses[0]?.phone}
+              </Text>
+              <Text pt="1" fontSize="0.8em">
+                <strong>Location: </strong>
+                {searchResults?.businesses[0]?.location.address1},{" "}
+                {searchResults?.businesses[0]?.location.city}
+              </Text>
             </VStack>
           </HStack>
         </Box>

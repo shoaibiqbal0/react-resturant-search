572f150b40f75ed2a513b13dacdc9158a04bfde7
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-26 18:56:41 +0100

    Added hover for result cards

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,4 +1,12 @@
-import { Badge, Box, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  HStack,
+  Image,
+  ScaleFade,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 
@@ -10,55 +18,57 @@ const ResultCard = ({ searchItem }) => {
     fontWeight: "normal",
   };
   return (
-    <Box
-      _hover={{
-        backgroundColor: "gray.50",
-        cursor: "pointer",
-      }}
-      borderRadius="5"
-      border="1px"
-      borderColor="gray.200"
-      boxShadow="lg"
-    >
-      <HStack p="1" align="flex-start" py="2">
-        <Image src={searchItem?.image_url} maxW="5em" borderRadius="5" />
-        <VStack spacing="0" w="100%" align="left">
-          <Text w="100%" fontWeight="bold" fontSize="2xl">
-            {searchItem?.name}
-          </Text>
-          <HStack w="100%">
-            <Badge style={badgeStyles}>
-              {searchItem?.categories[0]?.title}
-            </Badge>
-            <Badge style={badgeStyles}>
-              {searchItem?.categories[1]?.title}
-            </Badge>
-            <Text fontSize="0.6rem">{searchItem?.price}</Text>
-          </HStack>
+    <ScaleFade initialScale={0.9} in={true} whileHover={{ scale: 1.02 }}>
+      <Box
+        _hover={{
+          backgroundColor: "gray.50",
+          cursor: "pointer",
+        }}
+        borderRadius="5"
+        border="1px"
+        borderColor="gray.200"
+        boxShadow="lg"
+      >
+        <HStack p="1" align="flex-start" py="2">
+          <Image src={searchItem?.image_url} maxW="5em" borderRadius="5" />
+          <VStack spacing="0" w="100%" align="left">
+            <Text w="100%" fontWeight="bold" fontSize="2xl">
+              {searchItem?.name}
+            </Text>
+            <HStack w="100%">
+              <Badge style={badgeStyles}>
+                {searchItem?.categories[0]?.title}
+              </Badge>
+              <Badge style={badgeStyles}>
+                {searchItem?.categories[1]?.title}
+              </Badge>
+              <Text fontSize="0.6rem">{searchItem?.price}</Text>
+            </HStack>
 
-          <HStack pt="4" spacing="0">
-            <Text fontSize="0.8em">
-              <strong>Rating:</strong>
+            <HStack pt="4" spacing="0">
+              <Text fontSize="0.8em">
+                <strong>Rating:</strong>
+              </Text>
+              {[...Array(Math.floor(searchItem?.rating))].map((item, index) => {
+                return <AiFillStar color="orange" as="span" key={index} />;
+              })}
+            </HStack>
+            <HStack pt="1">
+              <Text fontSize="0.8em">
+                <strong>Phone:</strong>
+              </Text>
+              <Text textDecor="underline" textColor="blue.500" fontSize="0.8em">
+                <a href={`tel:${searchItem?.phone}`}>{searchItem?.phone}</a>
+              </Text>
+            </HStack>
+            <Text pt="1" fontSize="0.8em">
+              <strong>Location: </strong>
+              {searchItem?.location.address1}, {searchItem?.location.city}
             </Text>
-            {[...Array(Math.floor(searchItem?.rating))].map((item, index) => {
-              return <AiFillStar color="orange" as="span" key={index} />;
-            })}
-          </HStack>
-          <HStack pt="1">
-            <Text fontSize="0.8em">
-              <strong>Phone:</strong>
-            </Text>
-            <Text textDecor="underline" textColor="blue.500" fontSize="0.8em">
-              <a href={`tel:${searchItem?.phone}`}>{searchItem?.phone}</a>
-            </Text>
-          </HStack>
-          <Text pt="1" fontSize="0.8em">
-            <strong>Location: </strong>
-            {searchItem?.location.address1}, {searchItem?.location.city}
-          </Text>
-        </VStack>
-      </HStack>
-    </Box>
+          </VStack>
+        </HStack>
+      </Box>
+    </ScaleFade>
   );
 };
 

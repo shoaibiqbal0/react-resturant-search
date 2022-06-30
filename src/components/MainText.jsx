4962044fd859699de432417a6362c2ff9837e79c
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-06-30 10:32:43 +0100

    Removed font color

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -5,20 +5,10 @@ const MainText = () => {
   const subheadingFontSizes = ["2em", "3em", "4em"];
   return (
     <VStack py="1em">
-      <Text
-        fontSize={["4.5em", "6em", "7em"]}
-        fontWeight="extrabold"
-        mb="-5"
-      >
+      <Text fontSize={["4.5em", "6em", "7em"]} fontWeight="extrabold" mb="-5">
         WHERE
       </Text>
-      <Text
-        bgGradient="linear-gradient(to bottom right, red, yellow)"
-        bgClip="text"
-        fontSize={subheadingFontSizes}
-      >
-        do you wanna
-      </Text>
+      <Text fontSize={subheadingFontSizes}>do you wanna</Text>
       <Text
         bgGradient="radial-gradient(circle, rgba(142,8,79,1) 0%, rgba(255,0,172,1) 100%)"
         bgClip="text"

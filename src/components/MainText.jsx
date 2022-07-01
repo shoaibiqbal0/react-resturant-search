8e7b3265a2e06bb6dda0927fb98e26ee3fa0dbcc
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-01 14:13:20 +0100

    Added some bg

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -5,7 +5,13 @@ const MainText = () => {
   const subheadingFontSizes = ["2em", "3em", "4em"];
   return (
     <VStack py="1em">
-      <Text fontSize={["4.5em", "6em", "7em"]} fontWeight="extrabold" mb="-5">
+      <Text
+        bgGradient="linear(to-r, red, green, blue)"
+        bgClip="text"
+        fontSize={["4.5em", "6em", "7em"]}
+        fontWeight="extrabold"
+        mb="-5"
+      >
         WHERE
       </Text>
       <Text fontSize={subheadingFontSizes}>do you wanna</Text>

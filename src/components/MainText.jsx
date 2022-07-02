901ded3e56eb5b8cbf3ebe12d27a35a5126e2539
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-02 20:06:30 +0100

    Removed bg again

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -5,17 +5,10 @@ const MainText = () => {
   const subheadingFontSizes = ["2em", "3em", "4em"];
   return (
     <VStack py="1em">
-      <Text
-        bgClip="text"
-        fontSize={["4.5em", "6em", "7em"]}
-        fontWeight="extrabold"
-        mb="-5"
-      >
+      <Text fontSize={["4.5em", "6em", "7em"]} fontWeight="extrabold" mb="-5">
         WHERE
       </Text>
-      <Text bgClip="text" fontSize={subheadingFontSizes}>
-        do you wanna
-      </Text>
+      <Text fontSize={subheadingFontSizes}>do you wanna</Text>
       <Text fontSize={subheadingFontSizes}>
         <strong>EAT</strong> today?
       </Text>

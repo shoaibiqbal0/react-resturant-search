91795a7bf074d3826f325741bc98336b1259928b
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-03 18:43:21 +0100

    Removed bg again

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -8,9 +8,7 @@ const MainText = () => {
       <Text fontSize={["4.5em", "6em", "7em"]} fontWeight="extrabold" mb="-5">
         WHERE
       </Text>
-      <Text bgClip="text" fontSize={subheadingFontSizes}>
-        do you wanna
-      </Text>
+      <Text fontSize={subheadingFontSizes}>do you wanna</Text>
       <Text fontSize={subheadingFontSizes}>
         <strong>EAT</strong> today?
       </Text>

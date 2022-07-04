b2d688fb065ae04a2082d09ca93b4af28bae04ae
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-04 13:20:12 +0100

    Added bg image for main text

--- src/components/MainText.jsx
+++ src/components/MainText.jsx
@@ -1,11 +1,20 @@
 import { Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
+import textBg from "../assets/textBg.png";
+
 const MainText = () => {
   const subheadingFontSizes = ["2em", "3em", "4em"];
   return (
     <VStack py="1em">
-      <Text fontSize={["4.5em", "6em", "7em"]} fontWeight="extrabold" mb="-5">
+      <Text
+        bgImage={textBg}
+        bgSize="cover"
+        bgClip="text"
+        fontSize={["4.5em", "6em", "7em"]}
+        fontWeight="extrabold"
+        mb="-5"
+      >
         WHERE
       </Text>
       <Text fontSize={subheadingFontSizes}>do you wanna</Text>

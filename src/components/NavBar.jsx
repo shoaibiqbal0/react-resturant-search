f3a066fc5a7b8ab57953e4b7ea7e027c83b3888a
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-04 13:35:50 +0100

    Added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import { Flex, HStack, Image } from "@chakra-ui/react";
 import React from "react";
+import hamburger from "../assets/hamburger.png";
 
 const NavBar = () => {
   return (
-    <Flex>
-      <HStack>
-        <Image />
+    <Flex boxShadow="base">
+      <HStack p="3">
+        <Image w="70%" src={hamburger} />
       </HStack>
     </Flex>
   );

31afafc0284939e5152bc9ad3ff504491dd98473
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-04 13:41:06 +0100

    Added hover for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,18 @@
-import { Flex, HStack, Image } from "@chakra-ui/react";
+import { Flex, HStack, Image, ScaleFade } from "@chakra-ui/react";
 import React from "react";
 import hamburger from "../assets/hamburger.png";
 
 const NavBar = () => {
   return (
     <Flex boxShadow="base">
-      <HStack p="3">
-        <Image w="70%" src={hamburger} />
+      <HStack py="3" px="5">
+        <ScaleFade in={true} whileHover={{ scale: 1.1 }}>
+          <Image
+            _hover={{ cursor: "pointer" }}
+            w={["50%", "60%", "70%"]}
+            src={hamburger}
+          />
+        </ScaleFade>
       </HStack>
     </Flex>
   );

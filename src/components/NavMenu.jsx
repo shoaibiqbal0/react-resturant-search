b3bad301d6aba45cab6be3c90dbe116ae19585d4
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-07 19:34:10 +0100

    Menu  blur

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,11 +1,19 @@
-import { Box, Button, Flex, Slide } from "@chakra-ui/react";
+import { Box, Button, Slide } from "@chakra-ui/react";
+import { GrClose } from "react-icons";
 import React from "react";
 
 const NavMenu = ({ isOpen, onToggle }) => {
   return (
     <>
       <Slide direction="left" in={isOpen} style={{ zIndex: 10 }}>
-        <Box color="white" bg="teal.500" shadow="md" maxW="10rem" minH="100%">
+        <Box
+          color="white"
+          bg="hsl(0 0% 80% / 0.1)"
+          backdropFilter="blur(1rem)"
+          shadow="md"
+          maxW="10rem"
+          minH="100%"
+        >
           <Button onClick={onToggle}>Click Me</Button>
         </Box>
       </Slide>

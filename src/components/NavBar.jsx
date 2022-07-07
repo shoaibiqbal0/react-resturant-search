ce33e4c866ee9487ea299663513778ed5f1d54af
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-07 17:04:58 +0100

    Added menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,31 @@
-import { Flex, HStack, Image, ScaleFade } from "@chakra-ui/react";
+import {
+  Flex,
+  HStack,
+  Image,
+  ScaleFade,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 import hamburger from "../assets/hamburger.png";
+import NavMenu from "./NavMenu";
 
 const NavBar = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Flex boxShadow="base">
-      <HStack py="3" px="5">
-        <ScaleFade in={true} whileHover={{ scale: 1.2 }}>
-          <Image
-            _hover={{ cursor: "pointer" }}
-            w={["40%", "50%", "60%"]}
-            src={hamburger}
-          />
-        </ScaleFade>
-      </HStack>
-    </Flex>
+    <>
+      <Flex boxShadow="base">
+        <HStack py="3" px="5">
+          <ScaleFade in={true} whileHover={{ scale: 1.2 }}>
+            <Image
+              _hover={{ cursor: "pointer" }}
+              w={["40%", "50%", "60%"]}
+              src={hamburger}
+            />
+          </ScaleFade>
+        </HStack>
+      </Flex>
+      <NavMenu isOpen={isOpen} onToggle={onToggle} />
+    </>
   );
 };
 
--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,7 +1,22 @@
+import { Box, Button, Flex, Slide } from "@chakra-ui/react";
 import React from "react";
 
-const NavMenu = () => {
-  return <div>NavMenu</div>;
+const NavMenu = ({ isOpen, onToggle }) => {
+  return (
+    <>
+      <Slide direction="top" in={isOpen} style={{ zIndex: 10 }}>
+        <Button onClick={onToggle}>Click Me</Button>
+        <Box
+          p="40px"
+          color="white"
+          mt="4"
+          bg="teal.500"
+          rounded="md"
+          shadow="md"
+        ></Box>
+      </Slide>
+    </>
+  );
 };
 
 export default NavMenu;

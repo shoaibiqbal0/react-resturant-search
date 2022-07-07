d181afd431ca4d995e42f271a109880c2a77b1a3
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-07 19:51:44 +0100

    Finished menu

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,5 +1,5 @@
-import { Box, IconButton, Slide } from "@chakra-ui/react";
-import { ImCross } from "react-icons/im";
+import { Box, IconButton, SimpleGrid, Slide, Text } from "@chakra-ui/react";
+import { GrClose } from "react-icons/gr";
 import React from "react";
 
 const NavMenu = ({ isOpen, onToggle }) => {
@@ -7,21 +7,25 @@ const NavMenu = ({ isOpen, onToggle }) => {
     <>
       <Slide direction="left" in={isOpen} style={{ zIndex: 10 }}>
         <Box
-          color="white"
           bg="hsl(0 0% 80% / 0.1)"
           backdropFilter="blur(1rem)"
           shadow="md"
-          maxW="10rem"
+          maxW="15rem"
           minH="100vh"
         >
           <IconButton
             p="7"
-            icon={<ImCross size="1.5rem" />}
+            icon={<GrClose size="1.5rem" />}
             variant="link"
             onClick={onToggle}
           >
             Click Me
           </IconButton>
+          <SimpleGrid p="7" spacingY="5">
+            <Text fontSize="2xl">Home</Text>
+            <Text fontSize="2xl">Contact</Text>
+            <Text fontSize="2xl">About</Text>
+          </SimpleGrid>
         </Box>
       </Slide>
     </>

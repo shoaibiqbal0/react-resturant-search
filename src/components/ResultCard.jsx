a017451fcad9335e00e874fce3ec8b659340b7b2
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-11 14:04:03 +0100

    Added Call and Map buttons

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,11 +1,10 @@
 import {
   Badge,
   Box,
+  Button,
   HStack,
   Image,
-  ScaleFade,
   Text,
-  useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
@@ -13,70 +12,73 @@ import { AiFillStar } from "react-icons/ai";
 import emptyImg from "../assets/noimage.png";
 
 const ResultCard = ({ searchItem }) => {
+  "https://www.google.com/maps/place/56 The Market Sutton/@51.383233,-0.190163157672146";
+
   const badgeStyles = {
     borderRadius: "7px",
     fontSize: "0.6rem",
     textTransform: "capitalize",
     fontWeight: "normal",
   };
-  const hoverColor = useColorModeValue(
-    "hsla(0, 0%, 0%, 0.03)",
-    "hsla(0, 100%, 100%, 0.03)"
-  );
-  return (
-    <ScaleFade initialScale={0.9} in={true} whileHover={{ scale: 1.01 }}>
-      <Box
-        _hover={{
-          backgroundColor: hoverColor,
-          cursor: "pointer",
-          borderRadius: "5",
-        }}
-        boxShadow="lg"
-      >
-        <HStack p="1" align="flex-start" py="2">
-          <Image
-            src={searchItem.image_url === "" ? emptyImg : searchItem.image_url}
-            maxW="5em"
-            borderRadius="5"
-          />
-          <VStack spacing="0" w="100%" align="left">
-            <Text w="100%" fontWeight="bold" fontSize="2xl">
-              {searchItem?.name}
-            </Text>
-            <HStack w="100%">
-              <Badge style={badgeStyles}>
-                {searchItem?.categories[0]?.title}
-              </Badge>
-              <Badge style={badgeStyles}>
-                {searchItem?.categories[1]?.title}
-              </Badge>
-              <Text fontSize="0.6rem">{searchItem?.price}</Text>
-            </HStack>
 
-            <HStack pt="4" spacing="0">
-              <Text fontSize="0.8em">
-                <strong>Rating:</strong>
-              </Text>
-              {[...Array(Math.floor(searchItem?.rating))].map((item, index) => {
-                return <AiFillStar color="orange" as="span" key={index} />;
-              })}
-            </HStack>
-            <HStack pt="1">
-              <Text fontSize="0.8em">
-                <strong>Phone:</strong>
-              </Text>
-              <Text textDecor="underline" textColor="blue.500" fontSize="0.8em">
-                <a href={`tel:${searchItem?.phone}`}>{searchItem?.phone}</a>
-              </Text>
-            </HStack>
-            <Text pt="1" fontSize="0.8em">
-              <strong>Location: </strong>
-              {searchItem?.location.address1}, {searchItem?.location.city}
+  return (
+    <Box boxShadow="lg">
+      <HStack p="1" align="flex-start" py="2">
+        <Image
+          src={searchItem.image_url === "" ? emptyImg : searchItem.image_url}
+          maxW="5em"
+          borderRadius="5"
+        />
+        <VStack spacing="0" w="100%" align="left">
+          <Text w="100%" fontWeight="bold" fontSize="2xl">
+            {searchItem?.name}
+          </Text>
+          <HStack w="100%">
+            <Badge style={badgeStyles}>
+              {searchItem?.categories[0]?.title}
+            </Badge>
+            <Badge style={badgeStyles}>
+              {searchItem?.categories[1]?.title}
+            </Badge>
+            <Text fontSize="0.6rem">{searchItem?.price}</Text>
+          </HStack>
+
+          <HStack pt="4" spacing="0">
+            <Text fontSize="0.8em">
+              <strong>Rating:</strong>
             </Text>
-          </VStack>
-        </HStack>
-      </Box>
-    </ScaleFade>
+            {[...Array(Math.floor(searchItem?.rating))].map((item, index) => {
+              return <AiFillStar color="orange" as="span" key={index} />;
+            })}
+          </HStack>
+          <HStack pt="1">
+            <Text fontSize="0.8em">
+              <strong>Phone:</strong>
+            </Text>
+            <Text textDecor="underline" textColor="blue.500" fontSize="0.8em">
+              <a href={`tel:${searchItem?.phone}`}>{searchItem?.phone}</a>
+            </Text>
+          </HStack>
+          <Text pt="1" fontSize="0.8em">
+            <strong>Location: </strong>
+            {searchItem?.location.address1}, {searchItem?.location.city}
+          </Text>
+        </VStack>
+      </HStack>
+      <HStack justify="flex-end" p="2">
+        <Button as="a" colorScheme="blue" size="sm">
+          Map
+        </Button>
+        <Button
+          as="a"
+          href={`tel:${searchItem?.phone}`}
+          colorScheme="green"
+          size="sm"
+        >
+          Call
+        </Button>
+      </HStack>
+    </Box>
   );
 };
 

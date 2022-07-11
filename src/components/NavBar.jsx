bab06014675fa5b1ab3c69f55e560273d78b18c4
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-11 12:16:19 +0100

    Added sun and moon icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,35 @@
 import {
   Flex,
   HStack,
+  IconButton,
   Image,
   ScaleFade,
+  Spacer,
   useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import hamburger from "../assets/hamburger.png";
 import NavMenu from "./NavMenu";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { IconBase } from "react-icons/lib";
 
 const NavBar = () => {
   const { isOpen, onToggle } = useDisclosure();
   return (
     <>
-      <Flex boxShadow="base">
-        <HStack py="3" px="5">
-          <ScaleFade in={true} whileHover={{ scale: 1.2 }}>
-            <Image
-              onClick={onToggle}
-              _hover={{ cursor: "pointer" }}
-              w={["40%", "50%", "60%"]}
-              src={hamburger}
-            />
-          </ScaleFade>
-        </HStack>
-      </Flex>
+      <HStack py="3" px="5">
+        <ScaleFade in={true} whileHover={{ scale: 1.2 }}>
+          <Image
+            onClick={onToggle}
+            _hover={{ cursor: "pointer" }}
+            w={["40%", "50%", "60%"]}
+            src={hamburger}
+          />
+        </ScaleFade>
+        <Spacer />
+        <IconButton></IconButton>
+      </HStack>
+
       <NavMenu isOpen={isOpen} onToggle={onToggle} />
     </>
   );

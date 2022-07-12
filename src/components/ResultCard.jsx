ac3609e2d877372afe9cab56b0fb9e7a7165c59c
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-07-12 10:33:23 +0100

    Added some styling

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -16,7 +16,8 @@ const ResultCard = ({ searchItem }) => {
 
   const badgeStyles = {
     borderRadius: "7px",
-
+    fontSize: "0.6rem",
+    textTransform: "capitalize",
     fontWeight: "normal",
   };
 
